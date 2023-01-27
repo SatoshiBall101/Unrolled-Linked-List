f427ac99f6ad8f1b29c6bb82e51652fb4db4b395
Amman Chuhan <chuhan@usc.edu>
2023-01-27 18:28:41 +0000

    Completed ULLStr
    All tests passed!

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -2,12 +2,66 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <ostream>
 
 #include "ulliststr.h"
+
+using namespace std;
 
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+	ULListStr dat;
+	dat.push_back("7");
+	dat.push_front("8");
+	dat.push_back("9");
+	cout << "1." <<dat.back() << endl;
+	cout << "2." <<dat.front() << endl;
+	cout << "3." << dat.empty() << endl;
+	// prints: 8 7 9
+	cout <<  "4." <<dat.size() << endl;  // prints 3 since there are 3 strings stored
+
+	cout << "5." << dat.get(0) << endl;
+	cout << "6." << dat.get(1) << endl;
+	cout << "7." << dat.get(2) << endl; 
+
+	dat.pop_back();
+	dat.pop_front();
+	
+	cout << "8." << dat.get(0) << endl;
+
+	while(!dat.empty())
+	{
+		dat.pop_back();
+	}
+
+	cout << "The size is now: " << dat.size();
+
+	dat.push_back("Hi");
+	dat.push_back("THere");
+	dat.push_back("g");
+	dat.push_back("tgi");
+	dat.push_back("Hi");
+	dat.push_back("Hgt");
+	dat.push_back("vg");
+	dat.push_back("Hi");
+	dat.push_back("tfvHi");
+	dat.push_back("gt");
+	dat.push_back("vg");
+	dat.push_back("vg");
+
+
+	cout << endl;
+
+	for (int i = 0; i < 12; i++)
+	{
+		cout << dat.front() << endl;
+		dat.pop_front();
+	}
+	
+	
+
+
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -24,7 +24,142 @@ size_t ULListStr::size() const
   return size_;
 }
 
-// WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val)
+{
+	if(head_ == NULL && tail_ == NULL) // if the list is empty
+	{
+		Item* item1 = new Item; // dynamically allocate a new Item
+		head_ = tail_ = item1; // head and tail now point at the new Item
+		head_->val[0] = val; // place the value in the array
+		size_ = 1; // just created an item and placed a value, so size is 1
+		head_->last = 1; // move up last pointer 
+	}
+	else if(tail_->last < 10) // there is room to push_back value to last node
+	{
+		tail_->val[tail_->last] = val; // place the value in the array
+		size_++;
+		tail_->last++; //increment last index
+	}
+	else if (tail_->last == 10) // there is no room thus new item needed
+	{
+		Item* nextItem = new Item; // DA next Item 
+		Item* temp = tail_; // save previous last item's address
+		tail_ = nextItem; // tail is now pointing at next Item
+		tail_->prev = temp; // update nextItem's previous
+		tail_->prev->next = nextItem; // update the old tail's next to the next Item
+		tail_->val[0] = val; // store string
+		size_++; 
+		tail_->last++; // move up last index
+	}
+}
+
+void ULListStr::pop_back()
+{
+	if (size_ == 0)
+	{
+		return; // bc nothing to "pop" due to empty list
+	}
+
+	if (size_ == 1) // only 1 element in list so we can delete list
+	{
+		delete head_;
+		head_ = tail_ = NULL;
+		size_--;
+		
+	}
+	else
+	{
+		if (tail_->last == 1) // Item/node has single element  
+		{
+			tail_ = tail_->prev; // tail points at last non empty node
+			delete tail_->next; // delete the node with 1 item for popback
+			tail_->next = NULL; // drop off the empty Item from the LL
+			size_--;
+		}
+		else
+		{
+			tail_->last--;
+			size_--;
+		}
+	}
+}
+
+
+void ULListStr::push_front(const std::string& val)
+{
+	if(head_ == NULL && tail_ == NULL) // if the list is empty
+	{
+		Item* item2 = new Item; // dynamically allocate a new Item
+		head_ = tail_ = item2; // head and tail now point at the new Item
+		head_->val[9] = val; // place the value in the array
+		size_ = 1; // just created an item and placed a value, so size is 1
+		head_->first = 9; // first value at index 9 
+		head_->last = 10; // last value at index 10 (non inclusive)
+		return;
+	}
+
+	if(head_->first > 0) // there is room to push_front value to first node
+	{
+		head_->val[head_->first-1] = val; // place the value in the array
+		size_++;
+		head_->first--; //decrement first index 
+		return;
+	}
+	else if (head_->first == 0) // there is no room thus new item needed
+	{
+		Item* prevItem = new Item; // DA next Item 
+		Item* temp1 = head_; // save previous first item's address
+		head_ = prevItem; // head is now pointing at prevItem
+		head_->next = temp1; // update prevItem's next
+		head_->next->prev = prevItem; // update the old head's prev to the prevItem
+		head_->val[9] = val; // store string
+		size_++; 
+		head_->first = 9; // move up last index
+		head_->last = 10; // move up last index
+		return;
+	}
+
+}
+
+void ULListStr::pop_front()
+{
+	if (size_ == 0)
+	{
+		return; // bc nothing to "pop" due to empty list
+	}
+
+	if (size_ == 1) // only 1 element in list so we can delete list
+	{
+		delete head_;
+		head_ = tail_ = NULL;
+		size_--;
+	}
+	else
+	{
+		if (head_->first == 9) // Item/node has single element  
+		{
+			head_ = head_->next; // head points at first non empty node
+			delete head_->prev; // delete the old head node with 1 item for popfront
+			head_->prev = NULL; // drop off the empty Item from the LL
+			size_--;
+		}
+		else
+		{
+			head_->first++;
+			size_--;
+		}
+	}
+}
+
+std::string const & ULListStr::back() const
+{
+	return tail_->val[tail_->last-1];
+}
+
+std::string const & ULListStr::front() const
+{
+	return head_->val[head_->first];
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -33,6 +168,35 @@ void ULListStr::set(size_t loc, const std::string& val)
     throw std::invalid_argument("Bad location");
   }
   *ptr = val;
+}
+
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+	if (loc > size_)
+	{
+		return NULL;
+	}
+	else
+	{
+		Item* currNode = head_;
+		size_t currLoc = 0;
+		size_t currIdx = head_->first;
+		
+		while (currLoc != loc)
+		{
+			currLoc++;
+			currIdx++;
+			if (currIdx >= currNode->last)
+			{
+				currNode = currNode->next;
+				currIdx = currNode->first; // reset first 
+			}
+			
+		}
+
+		return &(currNode->val[currIdx]);
+	}
 }
 
 std::string& ULListStr::get(size_t loc)
